b9994c0363c18f3562847951f74250e4415f1bd7
Jayanta66 <jayhalder18@gmail.com>
2023-10-04 10:51:39 +0530

    This is web design project

---
+++ src/main/java/com/example/mymicroservices/MymicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymicroservicesApplication.class, args);
+	}
+
+}
